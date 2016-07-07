6919689739059d3dc20bce1b66acda80aebb2a2f
Chovin <ccarlson@docomopacific.com>
2016-07-07 11:47:12 +1000

    refactoring and stylistic choices
    I also propose we also use python 3.5 instead of 2 :P

--- main.py
+++ main.py
@@ -3,64 +3,50 @@ import random
 #data
 #greating 
 gt = [
-"It is (12:00) , may I suggest you something to eat ?",
-"How are how today ?",
-"Ask me anything ?",
-"What is your name ?",
-"Hello",
-"Hi there!",
-"How are you doing?",
-"I'm doing great.",
-"That is good to hear",
-"Thank you.",
-"Your welcome."
-]
-
-
+    "It is (12:00) , may I suggest you something to eat ?",
+    "How are how today ?",
+    "Ask me anything ?",
+    "What is your name ?",
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "Your welcome."
+    ]
 kw = [
-"fat",
-"bitch",
-"cunt",
-]
-
+    "fat",
+    "bitch",
+    "cunt"
+    ]
 kw_response = [
-"that is not nice ",
-"why are you saying that ",
-"very not nice ",
-]
-
+    "that is not nice ",
+    "why are you saying that ",
+    "very not nice "
+    ]
 ph = [
-"how old are you ?",
-"how can I assit you today ?",
-"Have seen our new service ?",
-"Do you know our most popular service ?",
-"Do ypu want to view the listing of our services ?",
-]
-
+    "how old are you ?",
+    "how can I assit you today ?",
+    "Have seen our new service ?",
+    "Do you know our most popular service ?",
+    "Do ypu want to view the listing of our services ?"
+    ]
 #
 def hello(n):
- txt = "Hello vistor , my name is Laetitia , I am a chatbot . I will assist you ."
- print txt + gt[n]
+    txt = "Hello vistor , my name is Laetitia , I am a chatbot . I will assist you ."
+    print txt + gt[n]
 
+# catch particular words
+def key_words(msg):
+    # do you want 'fattest' to trigger this?
+    return [random.choice(kw_response) for k in kw if k in msg][0]
 
-#catch particular words
-def KeyWords(wd):
- for i in wd.split(" "):
-  for ii in range (0,2):
-   if i==kw[ii]:
-    return kw_response[random.randint(0,2)]
- #check regular sentence
- 
+# check regular sentence
+hello (random.randint(1,3))  # this does not 'check' for anything? assume will be used later in an input loop
 
-
-hello (random.randint(1,3))
-
-
-
-
-print "Visitor :"
-txt = raw_input()
-print KeyWords(txt)
+txt = raw_input("Visitor :")
+print key_words(txt)
   
 
 
